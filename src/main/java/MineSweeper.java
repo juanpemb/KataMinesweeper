a7c98237a61bfc42ef5b20956a95e43948ddb21d
JuanPe <juanpedro.martinez.bruno.contractor@bbva.com>
2016-02-18 17:55:36 +0100

    resuelve una mina en un campo de 2x2

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -1,8 +1,33 @@
 
 public class MineSweeper {
 
-	public char[][] decode(char[][] input) {
-		return new char[1][1];		
+	private static final char MINE = '*';
+	private static final char ZERO = '0';
+	private static final char ONE = '1';
+
+	public char[][] decode(String input) {
+		int x = Integer.valueOf(input.substring(0,1));
+		int y = Integer.valueOf(input.substring(1,2));
+		char[][] result = new char[x][y];
+		int pos = 2;
+		for(int i = 0; i<x;i++)
+		{
+			for(int j = 0; j<x;j++)
+			{
+				if(input.contains("*")){
+					if(input.substring(pos,pos+1).equals("*")){
+						result[i][j] = MINE;
+					}else{
+					result[i][j] = ONE;
+					}
+				}				
+				else{
+					result[i][j] = ZERO;
+				}
+				pos++;
+			}
+		}
+		return result;		
 	}
 
 }
--- src/test/main/MineSweeperTest.java
+++ src/test/main/MineSweeperTest.java
@@ -3,16 +3,52 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MineSweeperTest {
-	private static final int _I = 1;
-	private static final int _J = 1;
+	private MineSweeper mineSweeper =  new MineSweeper();
+	private final String inputEmptyField= "22....";
+	private final String inputOneMineField= "22...*";
 
 	@Test
-	public void test(){		
-		char input[][] = new char[_I][_J];		
-		MineSweeper s =  new MineSweeper();
+	public void test_check_any_input(){		
 		
-		char[][] output = s.decode(input);
+		char[][] output = mineSweeper.decode(inputEmptyField);
 		
 		assertNotNull(output);
 	}
+	
+	@Test
+	public void test_obatin_input_without_mine(){
+		
+		char[][] decoded = mineSweeper.decode(inputEmptyField);
+		
+		assertArrayEquals(clear_field(), decoded);
+	}
+	
+	
+	@Test
+	public void test_obatin_input_with_one_mine(){
+		
+		char[][] decoded = mineSweeper.decode(inputOneMineField);
+		
+		assertArrayEquals(one_mine_field(), decoded);
+	}
+	
+
+	private Object[] one_mine_field() {
+		char[][] expetedResult = new char[2][2];
+		expetedResult[0][0] = '1';
+		expetedResult[0][1] = '1';
+		expetedResult[1][0] = '1';
+		expetedResult[1][1] = '*';
+		return expetedResult;
+	}
+
+	private char[][] clear_field() {
+		char[][] expetedResult = new char[2][2];
+		expetedResult[0][0] = '0';
+		expetedResult[0][1] = '0';
+		expetedResult[1][0] = '0';
+		expetedResult[1][1] = '0';
+		return expetedResult;
+	}
 }
+
